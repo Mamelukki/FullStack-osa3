d9544eb8b99131a28171904c31780be52797d1fb
Salmi Sara M <salmisar@cs.helsinki.fi>
2019-02-09 21:01:51 +0200

    Heroku-linkki README-tiedostoon

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FullStack-osa3
+
+https://blooming-wildwood-27092.herokuapp.com/
---
+++ build/precache-manifest.91e027d7ca9edb41bee59028a3460a31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b25bb2f98f188a91f24e",
+    "url": "/static/js/main.b25bb2f9.chunk.js"
+  },
+  {
+    "revision": "23feea3aedfdbea1a84a",
+    "url": "/static/js/1.23feea3a.chunk.js"
+  },
+  {
+    "revision": "b25bb2f98f188a91f24e",
+    "url": "/static/css/main.ee4ee6d6.chunk.css"
+  },
+  {
+    "revision": "3ed6fa87f83e996058ee115dd7e80b91",
+    "url": "/index.html"
+  }
+];
